7e2f455040cadd480558428f14daa43fe1f85c08
Jose Armando chavez <jos_ar07@hotmail.com>
2019-07-07 12:17:57 -0500

    Enter

--- chat/src/app/chat/chat.page.ts
+++ chat/src/app/chat/chat.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthenticationService } from '../authentication.service'
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.page.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPage implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
   }
-
+  logout() {
+    this.authService.logout();
+  }
 }
